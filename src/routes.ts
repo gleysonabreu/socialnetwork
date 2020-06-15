e8fde2c9d5d23c149dfd9b0d4ce8eb341ba9fd05
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-06-15 12:34:10 -0300

    Added routes for Comments.

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 
 import PostController from './controllers/PostController';
+import CommentController from './controllers/CommentController';
 
 const routes = Router();
 const postController = new PostController();
+const commentController = new CommentController();
 
 // Posts Routes
 routes.get('/post', postController.index);
 routes.post('/post', postController.create);
 routes.get('/post/:id', postController.show);
 routes.put('/post/:id', postController.update);
-routes.delete('/post/:id', postController.delete)
+routes.delete('/post/:id', postController.delete);
+
+// Comment Routes
+routes.get('/comment', commentController.create);
 
 
 export default routes;
