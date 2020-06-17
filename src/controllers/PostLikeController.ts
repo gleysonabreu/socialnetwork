4c7207154898611ea649d99cce41686ce0ddaedb
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-06-17 18:21:51 -0300

    Added the jwt token security

--- src/controllers/PostLikeController.ts
+++ src/controllers/PostLikeController.ts
@@ -1,26 +1,37 @@
+require('dotenv/config');
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import knex from '../database/connection';
+
+interface IToken{
+  data: {
+    id: number;
+  }
+}
 
 class PostLikeController{
 
   async create(request: Request, response: Response){
 
     const { authorization } = request.headers;
+    const tokenAuth = authorization?.split(' ')[1] || '';
     const { post_id } = request.body;
 
-    const dataLike = {
-      user_id: authorization,
-      post_id
-    }
+    try {
 
-    if( authorization ){
+      const decodedToken = <IToken>jwt.verify(tokenAuth, process.env.SECRET_KEY || '');
+      
+      const dataLike = {
+        user_id: decodedToken.data.id,
+        post_id
+      }
 
       await knex('post_like').insert(dataLike);
 
       return response.json({ success: true });
 
-    }else{
-      return response.status(400).json({ message: "Unauthenticated user." })
+    } catch (error) {
+      return response.status(400).json({ message: "Invalid token." });
     }
 
   }
@@ -45,19 +56,22 @@ class PostLikeController{
   async delete(request: Request, response: Response){
 
     const { authorization } = request.headers;
+    const tokenAuth = authorization?.split(' ')[1] || '';
     const { post_id } = request.params;
 
-    if( authorization ){
+    try {
+
+      const decodedToken = <IToken>jwt.verify(tokenAuth, process.env.SECRET_KEY || '');
 
       await knex('post_like')
-      .where('user_id', authorization)
+      .where('user_id', decodedToken.data.id)
       .andWhere('post_id', post_id)
       .del();
 
       return response.json({ success: true });
-
-    }else{
-      return response.status(400).json({ message: "Unauthenticated user." });
+      
+    } catch (error) {
+      return response.status(400).json({ message: "Invalid token." });
     }
 
   }
