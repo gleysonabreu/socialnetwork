bef13316702bf167f92452152c1e81089ea5e4f5
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-06-17 19:06:23 -0300

    Added the jwt token security.

--- src/controllers/CommentLikeController.ts
+++ src/controllers/CommentLikeController.ts
@@ -1,24 +1,36 @@
+require('dotenv/config');
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import knex from '../database/connection';
 
+interface IToken{
+  data: {
+    id: number;
+  }
+}
 class CommentLikeController{
   async create(request: Request, response: Response){
     const { authorization } = request.headers;
+    const tokenAuth = authorization?.split(' ')[1] || '';
     const { comment_id } = request.body;
 
-    const dataLike = {
-      user_id: authorization,
-      comment_id
-    }
+    try {
+      
+      const decodedToken = <IToken>jwt.verify(tokenAuth, process.env.SECRET_KEY||'');
 
-    if(authorization){
+      const dataLike = {
+        user_id: decodedToken.data.id,
+        comment_id
+      }
+
       await knex('comment_like').insert(dataLike);
 
       return response.json({ success: true });
 
-    }else{
-      return response.json({ message: "Unauthenticated user." });
+    } catch (error) {
+      return response.status(400).json({ message: "Invalid token." });
     }
+
   }
 
   async index(request: Request, response: Response){
@@ -39,20 +51,23 @@ class CommentLikeController{
 
   async delete(request: Request, response: Response){
     const { authorization } = request.headers;
+    const tokenAuth = authorization?.split(' ')[1] || '';
     const { comment_id } = request.params;
 
-    if( authorization ){
+    try {
+      const decodedToken = <IToken>jwt.verify(tokenAuth, process.env.SECRET_KEY||'');
 
       await knex('comment_like')
-      .where('user_id', authorization)
+      .where('user_id', decodedToken.data.id)
       .andWhere('comment_id', comment_id)
       .del();
 
       return response.json({ success: true });
-
-    }else{
-      return response.status(400).json({ message: "Unauthenticated user." });
+      
+    } catch ([error]) {
+      return response.status(400).json({ message: "Invalid token." });
     }
+
   }
 
 }
